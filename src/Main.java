07e0c0f1d3c100922ba97bf008d0df88ec6ad336
Milosz463 <miloszdawiec6@gmail.com>
2024-09-11 11:52:50 +0200

    skibidi

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //tablica
+        //musi miec z gory okreslony rozmiar
+        // nie mozna go pozniej zmienic
+        //elementem tablicy moga byc typy proste i zlozone
+        //tablica z wartosciamy losowym od 1 do 100 6 elementow
+        int tablicaLosowa[]= new int[6];
+        for (int i = 0; i < tablicaLosowa.length; i++) {
+            tablicaLosowa[i]=(int) (Math.random()*100+1);
+
+        }
+        //wypisywanie elementow tablicy
+        for (int element:tablicaLosowa) {
+            System.out.println(element+", ");
+        }
+    }
+}
